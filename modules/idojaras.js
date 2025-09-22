d12c5dfc66a0734cacada11a05e1e8bca4fb3a6d
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-09-22 09:55:07 +0200

    Backend fileok létrehozása, user js elkészítése, server js elkészítése, store js elkészítése

---
+++ modules/idojaras.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const { users, idojaras, getNextID, saveIdojaras } = require('../utils/store');
+
+
+
+
+module.exports = router;
